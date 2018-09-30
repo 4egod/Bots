b57d2b5fe7b09b76c01365a58c75242e3851bc8b
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-10-01 01:19:20 +0500

    Tests

--- Facebook.Tests/MessagingTests.cs
+++ Facebook.Tests/MessagingTests.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
 
 namespace Facebook.Tests
 {
     using Messaging;
-    using System.Diagnostics;
+    using Messaging.Buttons;
+    using Messaging.Templates;
+
     using static Consts;
 
     [TestClass]
@@ -76,5 +77,93 @@ namespace Facebook.Tests
             string result = client.RequestUserEmailAsync(UserId, "Your Email:").Result;
             Trace.WriteLine($"Message Id: {result}");
         }
+
+        [TestMethod]
+        public void GenericTemplateTest()
+        {
+            var buttons = new List<IButton>();
+            buttons.Add(new PostbackButton()
+            {
+                Title = "Button 1",
+                Payload = "webhook_value_1"
+            });
+
+            buttons.Add(new PostbackButton()
+            {
+                Title = "Button 2",
+                Payload = "webhook_value_2"
+            });
+
+            var defaultAction = new GenericTemplate.Element.Action()
+            {
+                Url = "https://m.me/",            
+            };
+
+            var elements = new List<GenericTemplate.Element>();
+            elements.Add(new GenericTemplate.Element()
+            {
+                Title = "Title 1",
+                Subtitle = "Subtitle 1",
+                ImageUrl = "",
+                DefaultAction = defaultAction,
+                Buttons = buttons
+            });
+
+            elements.Add(new GenericTemplate.Element()
+            {
+                Title = "Title 2",
+                Subtitle = "Subtitle 2",
+                ImageUrl = "",
+                Buttons = buttons
+            });
+
+            var attachment = new Attachment<GenericTemplate>()
+            {
+                Payload = new GenericTemplate()
+                {
+                    Elements = elements
+                }
+            };
+
+            string result = client.SendAttachment<Attachment<GenericTemplate>>(UserId, attachment).Result;
+            Trace.WriteLine($"Message Id: {result}");
+        }
+
+        [TestMethod]
+        public void ButtonTemplateTest()
+        {
+            var buttons = new List<IButton>();
+            buttons.Add(new UrlButton()
+            {
+                Title = "Url Button",
+                Url = "http://m.me"
+            });
+
+            buttons.Add(new PostbackButton()
+            {
+                Title = "Post Back Button",
+                Payload = "webhook_value"
+            });
+
+            buttons.Add(new CallButton()
+            {
+                Title = "Call button",
+                Payload = "+123456789"
+            });
+
+            var attachment = new Attachment<ButtonTemplate>()
+            {
+                Payload = new ButtonTemplate()
+                {
+                    Text = "Button Template Test",
+                    Buttons = buttons
+                }
+            };
+
+            string result = client.SendAttachment<Attachment<ButtonTemplate>>(UserId, attachment).Result;
+            Trace.WriteLine($"Message Id: {result}");
+        }
+
+
     }
 }
