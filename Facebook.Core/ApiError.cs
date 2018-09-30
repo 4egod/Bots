225b5a2f48790fd183b8b12c9bad52691918063d
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-10-01 01:07:59 +0500

    +Error/Exception Classes

---
+++ Facebook.Core/ApiError.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Facebook
+{
+    public class ApiError
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("fbtrace_id")]
+        public string TraceId { get; set; }
+    }
+
+    public class ApiErrorContainer
+    {
+        [JsonProperty("error")]
+        public ApiError Error { get; set; }
+    }
+}
---
+++ Facebook.Core/FacebookException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Facebook
+{
+    public class FacebookException : Exception
+    {
+        public FacebookException(ApiError error) : base($"[{error.Type}:{error.Code}]: {error.Message}") { }
+    }
+}
