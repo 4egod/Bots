3c4c9a2d674228811063d4d9922f683dabe99cd8
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-09-30 15:05:11 +0500

    +common classes

---
+++ Facebook.Core/ContentTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facebook
+{
+    public static class ContentTypes
+    {
+        public static string TextHtml => "text/html";
+
+        public static string ApplicationJson => "application/json";
+    }
+}
