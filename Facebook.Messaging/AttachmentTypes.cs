023ca2d43b4190ce00c2f1375935552777897dd9
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-10-01 01:18:38 +0500

    +Attachments

---
+++ Facebook.Messaging/AttachmentTypes.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Facebook.Messaging
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum AttachmentTypes
+    {
+        [EnumMember(Value = "image")]
+        Image,
+
+        [EnumMember(Value = "audio")]
+        Audio,
+
+        [EnumMember(Value = "video")]
+        Video,
+
+        [EnumMember(Value = "file")]
+        File,
+
+        [EnumMember(Value = "template")]
+        Template
+    }
+}
---
+++ Facebook.Messaging/IAttachment.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facebook.Messaging
+{
+    public interface IAttachment
+    {
+        AttachmentTypes AttachmentType { get; }
+    }
+}
