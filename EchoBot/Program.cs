96d51a08a6ac32f07dde31677ab0f813cc2a5603
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-09-30 15:04:46 +0500

    +bot definitions

---
+++ EchoBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EchoBot
+{
+    using static Facebook.Consts;
+    using Facebook.Bot;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+
+            BotClient bot = new BotClient(AppSecret, PageToken, VerifyToken);
+            bot.StartAsync();
+            bot.WaitForShutdown();
+        }
+    }
+}
---
+++ Facebook.Bot/BotClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Facebook.Bot
+{
+    using Messaging;
+    using Webhook;
+
+    public class BotClient
+    {
+        private MessagingClient messagingClient;
+
+        private WebhookServer webhook;
+
+        public BotClient(string appSecret, string pageToken, string verifyToken) : this(80, appSecret, pageToken, verifyToken) { }
+
+        public BotClient(int webhookPort, string appSecret, string pageToken, string verifyToken)
+        {
+            messagingClient = new MessagingClient(pageToken);
+            webhook = new WebhookServer(webhookPort, appSecret, verifyToken);
+        }
+
+        public void StartAsync()
+        {
+            webhook.StartAsync();
+        }
+
+        public void WaitForShutdown()
+        {
+            webhook.WaitForShutdown();
+        }
+    }
+}
