841191ed425b7cdb545425377cfc6a13eb7a0b11
為什麼 <andy840119@gmail.com>
2018-10-23 13:54:51 +0800

    add two property in UrlButton

--- Messenger.Bot/Buttons/UrlButton.cs
+++ Messenger.Bot/Buttons/UrlButton.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Messenger.Bot.Templates;
+using Newtonsoft.Json;
 
 namespace Messenger.Bot.Buttons
 {
@@ -11,5 +12,11 @@ namespace Messenger.Bot.Buttons
 
         [JsonProperty("url")]
         public string Url { get; set; }
+
+        [JsonProperty("webview_height_ratio")]
+        public WebViewHeightRatio WebViewHeightRatio { get; set; }
+
+        [JsonProperty("messenger_extensions")]
+        public bool MessengerExtensions { get; set; }
     }
 }
