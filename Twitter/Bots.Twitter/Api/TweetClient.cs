8687146e3b11de50c22315b98c20c5c7039e0345
Dmitry Tarasov <dmitry.tarasov@live.com>
2019-04-21 12:13:07 +0500

    nuget 1.19.4.21

--- Twitter/Bots.Twitter/Api/TweetClient.cs
+++ Twitter/Bots.Twitter/Api/TweetClient.cs
@@ -15,9 +15,30 @@ namespace Bots.Twitter.Api
 
         public override string ApiUri => base.ApiUri + "statuses/";
 
+        public async Task<Tweet> GetTweetAsync(long tweetId)
+        {
+            return await GetAsync<Tweet>(ApiUri + $"show.json?id={tweetId}");
+        }
+
         public async Task<Tweet> TweetAsync(string text)
         {
             return await PostAsync<Tweet>(null, ApiUri + $"update.json?status={Uri.EscapeDataString(text)}");
+        }
+
+        public async Task<Tweet> QuoteAsync(long tweetId, string text)
+        {
+            Tweet tweet = await GetTweetAsync(tweetId);
+
+            return await PostAsync<Tweet>(null, ApiUri + $"update.json?status=" +
+                $"{Uri.EscapeDataString(text + $" https://twitter.com/{tweet.Creator.ScreenName}/status/{tweetId}")}");
+        }
+
+        public async Task<Tweet> CommentAsync(long tweetId, string text)
+        {
+            Tweet tweet = await GetTweetAsync(tweetId);
+
+            return await PostAsync<Tweet>(null, ApiUri + $"update.json?status={Uri.EscapeDataString(text + " @4egod")}" +
+                $"&in_reply_to_status_id={tweetId}");
         }
     }
 }
